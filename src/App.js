847a1a631f96fb8d57549b9bb274c7afe5bd8378
sobirjon001 <sobirjon001@gmail.com>
2020-08-26 01:34:07 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+import Profile from "./Profile";
 
 function App() {
+  let greeting = <h1>Hellow World</h1>;
+  let detail = <p>name: Anna lastname: Bonna</p>;
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      {greeting}
+      {detail}
+      <Profile />
     </div>
   );
 }
---
+++ src/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is working</h1>
+        <p>Great job</p>
+      </div>
+    );
+  }
+}
+
+export default Profile;
